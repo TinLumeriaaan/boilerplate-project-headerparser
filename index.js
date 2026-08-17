f5e2cf1c9c8cdac50758460bfc68181069a5cd2d
Chrissydntes <christinedantes1604@gmail.com>
2026-08-17 10:50:12 +0800

    Added code from fcc challenge

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 // index.js
 // where your node app starts
 
-// init project
 require('dotenv').config();
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
+// enable CORS
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// http://expressjs.com/en/starter/static-files.html
+// static files
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// root route
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+// Request Header Parser Endpoint
+app.get('/api/whoami', function (req, res) {
+  res.json({
+    ipaddress: req.ip || req.headers['x-forwarded-for'],
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
 });
 
-// listen for requests :)
+// listen for requests
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
